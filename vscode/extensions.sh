7b027f832f627ba7378e13fd9d8f6519bed8277c
thiiagoms <thiagom.devsec@gmail.com>
2026-03-04 14:40:45 -0300

    feat: update vscode settings

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -18,66 +18,103 @@ ${RED}
  ██  ██       ██ ██      ██    ██ ██   ██ ██      
   ████   ███████  ██████  ██████  ██████  ███████ 
 ${ENDCOLOR}
-    [*] Author: Thiago Silva AKA thiiagoms
-    [*] E-mail: thiagom.devsec@gmail.com
+    [*] Author: thiiagoms
+    [*] E-mail: thiiagoms@proton.me
 "
 
 extensions=(
-    adpyke.codesnap
-    alefragnani.project-manager
-    amiralizadeh9480.laravel-extra-intellisense
+    adpyke.vscode-sql-formatter
+    alexcvzz.vscode-sqlite
+    alperenersoy.filament-snippets
     bmewburn.vscode-intelephense-client
-    bradlc.vscode-tailwindcss
-    brapifra.phpserver
+    bwya77.islands-dark
+    christian-kohler.path-intellisense
     cjhowe7.laravel-blade
-    codingyu.laravel-goto-view
+    cweijan.dbclient-jdbc
+    cweijan.vscode-database-client2
+    davidanson.vscode-markdownlint
+    dineug.vuerd-vscode
+    docker.docker
     donjayamanne.githistory
+    dotjoshjohnson.xml
     eamodio.gitlens
-    EditorConfig.EditorConfig
-    evan-buss.font-switcher
+    ecmel.vscode-html-css
+    esafirm.kotlin-formatter
+    esbenp.prettier-vscode
     formulahendry.auto-close-tag
+    formulahendry.auto-complete-tag
     formulahendry.auto-rename-tag
-    GitHub.vscode-pull-request-github
-    glitchbl.laravel-create-view
-    hbenl.vscode-test-explorer
-    IHunte.laravel-blade-wrapper
-    ii3i.min
-    junstyle.php-cs-fixer
-    liamhammett.inline-parameters
-    m1guelpf.better-pest
+    formulahendry.code-runner
+    fwcd.kotlin
+    github.copilot-chat
+    github.vscode-github-actions
+    github.vscode-pull-request-github
+    golang.go
+    hollowtree.vue-snippets
+    jaguadoromero.vscode-php-create-class
     marabesi.php-import-checker
-    MehediDracula.php-namespace-resolver
+    mathiasfrohlich.kotlin
+    mechatroner.rainbow-csv
+    mehedidracula.php-namespace-resolver
+    mhutchie.git-graph
     miguelsolorio.fluent-icons
     mikestead.dotenv
+    ms-azuretools.vscode-containers
     ms-azuretools.vscode-docker
-    ms-kubernetes-tools.vscode-kubernetes-tools
-    ms-python.isort
+    ms-dotnettools.csdevkit
+    ms-dotnettools.csharp
+    ms-dotnettools.vscode-dotnet-runtime
+    ms-python.debugpy
     ms-python.python
     ms-python.vscode-pylance
+    ms-python.vscode-python-envs
     ms-toolsai.jupyter
     ms-toolsai.jupyter-keymap
     ms-toolsai.jupyter-renderers
     ms-toolsai.vscode-jupyter-cell-tags
     ms-toolsai.vscode-jupyter-slideshow
     ms-vscode-remote.remote-containers
-    ms-vscode.test-adapter-converter
+    ms-vscode-remote.remote-ssh
+    ms-vscode-remote.remote-ssh-edit
+    ms-vscode.makefile-tools
+    ms-vscode.remote-explorer
     ms-vscode.vscode-typescript-next
-    naoray.laravel-goto-components
+    naumovs.color-highlight
     neilbrayfield.php-docblocker
+    nucllear.vscode-extension-auto-import
     oderwat.indent-rainbow
     onecentlin.laravel-blade
-    onecentlin.laravel5-snippets
-    patbenatar.advanced-new-file
-    rangav.vscode-thunder-client
-    redhat.vscode-yaml
+    pnp.polacode
+    redhat.java
+    redhat.vscode-xml
     rifi2k.format-html-in-php
-    ryannaddy.laravel-artisan
+    ritwickdey.liveserver
+    ryanluker.vscode-coverage-gutters
+    shengchen.vscode-checkstyle
     shufo.vscode-blade-formatter
-    sleistner.vscode-fileutils
+    sonarsource.sonarlint-vscode
     streetsidesoftware.code-spell-checker
-    Tyriar.lorem-ipsum
+    subframe7536.custom-ui-style
+    talhabalaj.actual-font-changer
+    tomoki1207.pdf
+    vmware.vscode-spring-boot
+    vscjava.migrate-java-to-azure
+    vscjava.vscode-gradle
+    vscjava.vscode-java-debug
+    vscjava.vscode-java-dependency
+    vscjava.vscode-java-pack
+    vscjava.vscode-java-test
+    vscjava.vscode-java-upgrade
+    vscjava.vscode-lombok
+    vscjava.vscode-maven
+    vscjava.vscode-spring-boot-dashboard
+    vscjava.vscode-spring-initializr
     vscode-icons-team.vscode-icons
-    wraith13.file-path-bar
+    vue.volar
+    wayou.vscode-todo-highlight
+    whatwedo.twig
+    wix.vscode-import-cost
+    xdebug.php-debug
 )
 
 for extension in "${extensions[@]}"
